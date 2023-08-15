62c473524489cc95745948a8240c86cef7559101
Jordi2010 <127571316+Jordi2010@users.noreply.github.com>
2023-08-15 14:21:06 -0600

    Add files via upload

---
+++ Práctica 2/Practica2Demo/Practica2UI/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Practica2UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Antes del evento";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Antes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(186, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Después";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(516, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 130);
+            this.panel1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(551, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cambiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Practica2UI.Properties.Resources.c_sharp_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(102, 250);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(29, 387);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 42);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Anterior";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(201, 387);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Siguiente";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Problema 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(573, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Problema 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Problema 3 - Ejercicio de la práctica";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "c-sharp_logo.png");
+            this.imageList1.Images.SetKeyName(1, "Python_logo.png");
+            this.imageList1.Images.SetKeyName(2, "top-software-companies.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Práctica 2/Practica2Demo/Practica2UI/Form1.cs
@@ -0,0 +1,80 @@
+﻿/*Práctica 2 - Grupo 1
+  Asignatura: Programación orientada a eventos.    Sección: "A".
+  Estudiante: Jordi Haziel Amaya Martínez.*/
+
+//extensiones utilizadas (algunas).
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//nombre del proyecto.
+namespace Practica2UI
+{
+    public partial class Form1 : Form
+    {
+        //inicializando formulario.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //"PROBLEMA 1"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*tomar etiqueta y asignarle un cambio en el contenido mediante el evento click del botón.
+            (El label siempre es .Text)*/
+            label1.Text = "Antes del evento";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //tomar etiqueta y asignarle un cambio en el contenido mediante el evento click del botón.
+            label1.Text = "Después del evento";
+        }
+
+        //"PROBLEMA 2"
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*modificando el panel para que cambie de color aleatoriamente con backColor y propiedad
+            Color.FromArgb cambiamos de color y con new random le generamos un color aleatorio.
+            .Next asignamos valor siguiente.*/
+            Random random = new Random();
+            panel1.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        //"PROBLEMA 3" - Ejercicio de la práctica
+        //creando e inicializando un contador en 0 para controlar las imágenes (posición) a mostrar.
+        int contador = 0;
+        //añadiendo el evento del botón "Siguiente" al presionarlo.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //evalúa si puede pasar a las imágenes siguientes (por su número de posición).
+            if (contador < 2)
+            {
+                //cuenta y suma el número de posición de las imagénes.
+                contador++;
+            }
+            //muestra en el pictureBox la imagen de la posición que tiene el contador.
+            pictureBox1.Image = imageList1.Images[contador];
+        }
+        //añadiendo el evento del botón "Anterior" al presionarlo.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //evalúa si puede retroceder a las imágenes anteriores (por su número de posición).
+            if (contador > 0)
+            {
+                //cuenta y resta el número de posición de las imagénes.
+                contador--;
+            }
+            //muestra en el pictureBox la imagen de la posición que tiene el contador.
+            pictureBox1.Image = imageList1.Images[contador];
+        }
+    }
+}
