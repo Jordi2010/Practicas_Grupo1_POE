2ec7ad1ede5de0383be6f6356453c57c77849ac1
Jordi2010 <127571316+Jordi2010@users.noreply.github.com>
2023-11-13 14:29:37 -0600

    Add files via upload

---
+++ Práctica 13, POE/Login/LoginC#/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace LoginC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            txtUsuario = new TextBox();
+            txtPass = new TextBox();
+            btnIniciar = new Button();
+            btnPass2 = new LinkLabel();
+            pictureBoxMinimizar = new PictureBox();
+            pictureBoxCerrar = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCerrar).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.HotTrack;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 450);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(24, 122);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(152, 166);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.HighlightText;
+            label1.Location = new Point(418, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 39);
+            label1.TabIndex = 1;
+            label1.Text = "Mi Login";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.BackColor = SystemColors.InactiveCaptionText;
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Font = new Font("Century Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUsuario.ForeColor = SystemColors.Window;
+            txtUsuario.Location = new Point(306, 106);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(395, 19);
+            txtUsuario.TabIndex = 2;
+            txtUsuario.Text = "USUARIO";
+            txtUsuario.Enter += txtUsuario_Enter;
+            txtUsuario.Leave += txtUsuario_Leave;
+            // 
+            // txtPass
+            // 
+            txtPass.BackColor = SystemColors.InactiveCaptionText;
+            txtPass.BorderStyle = BorderStyle.None;
+            txtPass.Font = new Font("Century Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPass.ForeColor = SystemColors.Window;
+            txtPass.Location = new Point(306, 210);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(395, 19);
+            txtPass.TabIndex = 3;
+            txtPass.Text = "CONTRASEÑA";
+            txtPass.Enter += txtPass_Enter;
+            txtPass.Leave += txtPass_Leave;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.BackColor = Color.Black;
+            btnIniciar.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnIniciar.ForeColor = SystemColors.ControlLightLight;
+            btnIniciar.Location = new Point(442, 302);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(123, 30);
+            btnIniciar.TabIndex = 4;
+            btnIniciar.Text = "INICIAR SESIÓN";
+            btnIniciar.UseVisualStyleBackColor = false;
+            // 
+            // btnPass2
+            // 
+            btnPass2.AutoSize = true;
+            btnPass2.LinkColor = Color.Gray;
+            btnPass2.Location = new Point(420, 383);
+            btnPass2.Name = "btnPass2";
+            btnPass2.Size = new Size(161, 15);
+            btnPass2.TabIndex = 5;
+            btnPass2.TabStop = true;
+            btnPass2.Text = "¿Has olvidado tu contraseña?";
+            // 
+            // pictureBoxMinimizar
+            // 
+            pictureBoxMinimizar.BackColor = Color.White;
+            pictureBoxMinimizar.Image = (Image)resources.GetObject("pictureBoxMinimizar.Image");
+            pictureBoxMinimizar.Location = new Point(724, 6);
+            pictureBoxMinimizar.Name = "pictureBoxMinimizar";
+            pictureBoxMinimizar.Size = new Size(32, 30);
+            pictureBoxMinimizar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxMinimizar.TabIndex = 6;
+            pictureBoxMinimizar.TabStop = false;
+            pictureBoxMinimizar.Click += pictureBoxMinimizar_Click;
+            // 
+            // pictureBoxCerrar
+            // 
+            pictureBoxCerrar.BackColor = Color.White;
+            pictureBoxCerrar.Image = (Image)resources.GetObject("pictureBoxCerrar.Image");
+            pictureBoxCerrar.Location = new Point(762, 6);
+            pictureBoxCerrar.Name = "pictureBoxCerrar";
+            pictureBoxCerrar.Size = new Size(32, 30);
+            pictureBoxCerrar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxCerrar.TabIndex = 7;
+            pictureBoxCerrar.TabStop = false;
+            pictureBoxCerrar.Click += pictureBoxCerrar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(15, 15, 15);
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBoxCerrar);
+            Controls.Add(pictureBoxMinimizar);
+            Controls.Add(btnPass2);
+            Controls.Add(btnIniciar);
+            Controls.Add(txtPass);
+            Controls.Add(txtUsuario);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.9D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCerrar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox txtUsuario;
+        private TextBox txtPass;
+        private Button btnIniciar;
+        private LinkLabel btnPass2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBoxMinimizar;
+        private PictureBox pictureBoxCerrar;
+    }
+}
---
+++ Práctica 13, POE/Login/LoginC#/Form1.cs
@@ -0,0 +1,58 @@
+namespace LoginC_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.RoyalBlue;
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "CONTRASEÑA")
+            {
+                txtPass.Text = "";
+                txtPass.ForeColor = Color.RoyalBlue;
+                txtPass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "CONTRASEÑA";
+                txtPass.ForeColor = Color.DimGray;
+                txtPass.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void pictureBoxCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBoxMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
